a7f382831c1063d34f4fea67946a5dc52f356ded
RobertAndreasFritsch <fritsch-robert@web.de>
2017-05-28 18:25:16 +0200

    theme init

--- cms/src/org/cms/theme/index.php
+++ cms/src/org/cms/theme/index.php
@@ -1,4 +1,34 @@
 <?php
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php  ?></title>
+  </head>
+  <body>
+    <header>
 
-/* TODO */
+    </header>
+    <nav>
+
+    </nav>
+    <main>
+      <section>
+
+      </section>
+      <article class="">
+
+      </article>
+      <section>
+
+      </section>
+      <aside class="">
+
+      </aside>
+    </main>
+    <footer>
+
+    </footer>
+  </body>
+</html>
