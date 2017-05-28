840c195ccdb1d13621c317ddb91584cf5afadfca
Eliashuebner <eliashuebner@online.de>
2017-05-28 18:36:17 +0200

    platz

--- cms/src/org/cms/theme/index.php
+++ cms/src/org/cms/theme/index.php
@@ -5,16 +5,20 @@ use src\org\cms\page\Page;
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title><?php Page::get("author") ?></title>
+
+  <meta charset="utf-8">
+  <title><?php Page::get("author") ?></title>
+
 </head>
 <body>
-	<header></header>
-	<nav></nav>
-	<section></section>
-	<article></article>
-	<section></section>
-	<aside></aside>
-	<footer></footer>
+
+  <header></header>
+  <nav></nav>
+  <section></section>
+  <article></article>
+  <section></section>
+  <aside></aside>
+  <footer></footer>
+  
 </body>
 </html>
